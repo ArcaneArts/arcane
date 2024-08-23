75db8ec459009ed2b53c532def12ad1e597d0a23
cyberpwn <danielmillst@gmail.com>
2024-08-23 14:29:34 -0400

    f

--- lib/arcane.dart
+++ lib/arcane.dart
@@ -1,12 +1,1 @@
 library arcane;
-
-export 'package:arcane/arcane_external.dart';
-export 'package:arcane/feature/application/arcane_app.dart';
-export 'package:arcane/feature/application/initializer.dart';
-export 'package:arcane/feature/application/router.dart';
-export 'package:arcane/feature/application/widget/background.dart';
-export 'package:arcane/feature/application/widget/opal.dart';
-export 'package:arcane/feature/application/widget/title_bar.dart';
-export 'package:arcane/feature/application/widget/unicorn.dart';
-export 'package:arcane/feature/application/widget/wrapper.dart';
-export 'package:arcane/feature/util/firebase.dart';
--- lib/feature/util/firebase.dart
+++
@@ -1,6 +0,0 @@
-import 'package:arcane/arcane.dart';
-
-extension XString on String {
-  DocumentReference<Map<String, dynamic>> get doc =>
-      FirebaseFirestore.instance.doc(this);
-}
