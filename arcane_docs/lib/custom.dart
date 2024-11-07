eba34f29a72be8ae66e620115de26b749a002019
cyberpwn <danielmillst@gmail.com>
2024-11-07 05:17:33 -0500

    Screen nav docs

--- arcane_docs/lib/custom.dart
+++ arcane_docs/lib/custom.dart
@@ -241,10 +241,153 @@ SliverScreen(
                   ],
                 )),
           ),
-        )
+        ),
+        ArcaneUsageExample(
+          padding: 0,
+          title: 'Navigation Screens',
+          code: """
+NavigationScreen(
+  type: type,
+  index: index,
+  onIndexChanged: (index) => setState(() => this.index = index),
+  tabs: [
+    NavTab(
+        label: "Tab 1",
+        icon: Icons.activity,
+        selectedIcon: Icons.activity_fill,
+        builder: (context, footer) => SliverScreen(
+            gutter: false,
+            footer: footer,
+            header: Bar(titleText: "Tab 1"),
+            sliver: SListView.builder(
+                childCount: 10,
+                builder: (context, i) => ListTile(
+                      title: Text("Item $i"),
+                      subtitle: Text("Subtitle or something"),
+                      leading: Icon(Icons.activity),
+                    )))),
+    NavTab(
+        label: "Tab 2",
+        icon: Icons.address_book,
+        selectedIcon: Icons.address_book_fill,
+        builder: (context, footer) => SliverScreen(
+            gutter: false,
+            footer: footer,
+            header: Bar(titleText: "Tab 2"),
+            sliver: SGridView.builder(
+                crossAxisCount: 3,
+                childCount: 10,
+                builder: (context, i) => Card(
+                      child: Basic(
+                        title: Text("Item $i"),
+                        subtitle: Text("Subtitle or something"),
+                        leading: Icon(Icons.activity),
+                      ),
+                    )))),
+    NavTab(
+        label: "Tab 3",
+        icon: Icons.gear_six,
+        selectedIcon: Icons.gear_six_fill,
+        builder: (context, footer) => FillScreen(
+            footer: footer,
+            gutter: false,
+            header: Bar(titleText: "Tab 3"),
+            child: Center(
+              child: CardCarousel(
+                children: [
+                  RadioCards<NavigationType>(
+                      builder: (nt) => Basic(title: Text(nt.name)),
+                      onChanged: (nt) => setState(() => type = nt),
+                      items: NavigationType.values,
+                      value: type)
+                ],
+              ),
+            )))
+  ]
+)
+""",
+          child: SizedBox(
+            height: 500,
+            child: ExampleNavigationScreen(),
+          ),
+        ),
       ],
     );
   }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+class ExampleNavigationScreen extends StatefulWidget {
+  const ExampleNavigationScreen({super.key});
+
+  @override
+  State<ExampleNavigationScreen> createState() =>
+      _ExampleNavigationScreenState();
+}
+
+class _ExampleNavigationScreenState extends State<ExampleNavigationScreen> {
+  NavigationType type = NavigationType.bottomNavigationBar;
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) => NavigationScreen(
+          type: type,
+          index: index,
+          onIndexChanged: (index) => setState(() => this.index = index),
+          tabs: [
+            NavTab(
+                label: "Tab 1",
+                icon: Icons.activity,
+                selectedIcon: Icons.activity_fill,
+                builder: (context, footer) => SliverScreen(
+                    gutter: false,
+                    footer: footer,
+                    header: Bar(titleText: "Tab 1"),
+                    sliver: SListView.builder(
+                        childCount: 10,
+                        builder: (context, i) => ListTile(
+                              title: Text("Item $i"),
+                              subtitle: Text("Subtitle or something"),
+                              leading: Icon(Icons.activity),
+                            )))),
+            NavTab(
+                label: "Tab 2",
+                icon: Icons.address_book,
+                selectedIcon: Icons.address_book_fill,
+                builder: (context, footer) => SliverScreen(
+                    gutter: false,
+                    footer: footer,
+                    header: Bar(titleText: "Tab 2"),
+                    sliver: SGridView.builder(
+                        crossAxisCount: 3,
+                        childCount: 10,
+                        builder: (context, i) => Card(
+                              child: Basic(
+                                title: Text("Item $i"),
+                                subtitle: Text("Subtitle or something"),
+                                leading: Icon(Icons.activity),
+                              ),
+                            )))),
+            NavTab(
+                label: "Tab 3",
+                icon: Icons.gear_six,
+                selectedIcon: Icons.gear_six_fill,
+                builder: (context, footer) => FillScreen(
+                    footer: footer,
+                    gutter: false,
+                    header: Bar(titleText: "Tab 3"),
+                    child: Center(
+                      child: CardCarousel(
+                        children: [
+                          RadioCards<NavigationType>(
+                              builder: (nt) => Basic(title: Text(nt.name)),
+                              onChanged: (nt) => setState(() => type = nt),
+                              items: NavigationType.values,
+                              value: type)
+                        ],
+                      ),
+                    )))
+          ]);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
