d2a8e47721d586bbfc0cfd1a091029d101c97f14
cyberpwn <danielmillst@gmail.com>
2024-11-06 22:46:45 -0500

    Fix scripts

--- sync_shadcn.sh
+++ sync_shadcn.sh
@@ -28,33 +28,36 @@ echo "Fixing imports for 'arcane_shadcn'."
 find lib/generated/arcane_shadcn -type f -name "*.dart" -exec sed -i 's|import '\''package:shadcn_flutter/|import '\''package:arcane/generated/arcane_shadcn/|g' {} +
 
 # Tear out docs into arcane
-echo "Tearing out 'arcane_shadcn/docs' into 'intermediate_docs'."
-rm -rf intermediate_docs && mkdir -p intermediate_docs
-cp -a arcane_shadcn/docs/. intermediate_docs 
+echo "Tearing out 'arcane_shadcn/docs' into 'docs'."
+rm -rf docs && mkdir -p docs
+cp -a arcane_shadcn/docs/. docs 
 
 # Fix doc imports
-echo "Fixing pubspec for 'intermediate_docs' to target arcane_shadcn."
-find intermediate_docs -type f -name "pubspec.yaml" -exec sed -i 's|path: ../|path: ../arcane_shadcn|g' {} +
-echo "Updating dependencies for 'intermediate_docs'."
+echo "Fixing pubspec for 'docs' to target arcane_shadcn."
+find docs -type f -name "pubspec.yaml" -exec sed -i 's|path: ../|path: ../arcane_shadcn|g' {} +
+find docs -type f -name "pubspec.yaml" -exec sed -i 's|  # The following adds the Cupertino Icons font to your application.|  arcane: |g' {} +
+find docs -type f -name "pubspec.yaml" -exec sed -i 's|  # Use with the CupertinoIcons class for iOS style icons.|    path: ../ |g' {} +
+echo "Updating dependencies for 'docs'."
 
 # Brand docs
-cd intermediate_docs
-echo "Branding 'intermediate_docs' to 'arcane'."
+cd docs
+echo "Branding 'docs' to 'arcane'."
 find lib/pages -type f -name "docs_page.dart" -exec sed -i "s/'shadcn_flutter'/'arcane'/g" {} +
 cd ..
 echo "Changing urls to arcane"
-find intermediate_docs -type f -name "*.dart" -exec sed -i 's|'https://github.com/sunarya-thito/shadcn_flutter'|'https://github.com/ArcaneArts/arcane'|g' {} +
-find intermediate_docs -type f -name "*.dart" -exec sed -i 's|'https://pub.dev/packages/shadcn_flutter'|'https://pub.dev/packages/arcane'|g' {} +
+find docs -type f -name "*.dart" -exec sed -i 's|'https://github.com/sunarya-thito/shadcn_flutter'|'https://github.com/ArcaneArts/arcane'|g' {} +
+find docs -type f -name "*.dart" -exec sed -i 's|'https://pub.dev/packages/shadcn_flutter'|'https://pub.dev/packages/arcane'|g' {} +
+cp arcane_docs/lib/custom.dart docs/lib/custom.dart
 
 # Cleanup
-echo "Cleaning intermediate_docs & example remnants."
+echo "Cleaning docs & example remnants."
 rm -rf arcane_shadcn/docs &
 rm -rf arcane_shadcn/example &
 rm -rf arcane_shadcn/docs_images &
 rm -rf arcane_shadcn/.github &
 echo "Pub get on all projects."
 (flutter clean && flutter pub get) &
-(cd intermediate_docs && flutter clean && flutter pub get) &
+(cd docs && flutter clean && flutter pub get) &
 (cd example && flutter clean && flutter pub get) &
 (cd arcane_shadcn && flutter clean && flutter pub get) &
 wait
