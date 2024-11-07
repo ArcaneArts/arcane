6484b891bbea538babb2e5bdeb4759f9098500fd
cyberpwn <danielmillst@gmail.com>
2024-11-06 20:15:08 -0500

    Update script patcher

--- sync_shadcn.sh
+++ sync_shadcn.sh
@@ -2,14 +2,14 @@
 
 REPO="ArcaneArts/arcane_shadcn"
 BRANCH="arcane"
-FOLDER_NAME="arcane_shadcn"
 
-if [ ! -d "$FOLDER_NAME" ]; then
+# Update Git
+if [ ! -d "arcane_shadcn" ]; then
     echo "Repository doesn't exist locally. Cloning..."
     git clone -b $BRANCH https://github.com/ArcaneArts/arcane_shadcn.git
 else
     echo "Repository exists. Updating..."
-    cd "$FOLDER_NAME"
+    cd "arcane_shadcn"
     git fetch origin
     git reset --hard "origin/$BRANCH"
     cd ..
@@ -17,4 +17,20 @@ fi
 
 echo "Done! Repository is up to date on branch '$BRANCH'."
 
-pause
+# Update dependencies
+cd "arcane_shadcn"
+flutter pub get
+cd docs && flutter pub get && cd ..
+cd example && flutter pub get && cd ..
+cd ..
+
+# Install package
+rm -rf lib/generated/arcane_shadcn
+mkdir -p lib/generated/arcane_shadcn
+cp -a arcane_shadcn/lib/. lib/generated/arcane_shadcn
+
+# Fix imports
+find lib/generated/arcane_shadcn -type f -name "*.dart" -exec sed -i 's|import '\''package:shadcn_flutter/|import '\''package:arcane/generated/arcane_shadcn/|g' {} +
+
+# Update arcane dependencies
+flutter pub get
