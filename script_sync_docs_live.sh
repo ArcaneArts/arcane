feb261fe45bef7b6ac14490f8d6e13422f36941c
cyberpwn <danielmillst@gmail.com>
2024-11-08 12:01:30 -0500

    Scripts

--- script_sync_docs_live.sh
+++ script_sync_docs_live.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 cp arcane_docs/lib/custom.dart docs/lib/custom.dart
+cp arcane_docs/lib/installation_page.dart docs/lib/pages/docs/installation_page.dart
+cp arcane_docs/lib/introduction_page.dart docs/lib/pages/docs/introduction_page.dart
