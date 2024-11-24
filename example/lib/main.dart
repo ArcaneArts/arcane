63a5c7a5e3d7f070032c6b858ccb85612a341318
cyberpwn <danielmillst@gmail.com>
2024-11-24 04:43:47 -0500

    Tests

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:arcane/arcane.dart';
-import 'package:arcane/component/dialog/command.dart';
 
 bool v = false;
 String? vv;
@@ -12,31 +11,7 @@ class ExampleArcaneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ArcaneApp(
-        home: FillScreen(
-            header: Bar(
-              titleText: "This is bar",
-              trailing: [
-                SearchButton(
-                  mode: SearchButtonMode.live,
-                  onSearch: (v) {
-                    print("SEARCH CALLED '$v'");
-                  },
-                )
-              ],
-            ),
-            child: Center(
-              child: Builder(
-                  builder: (context) => PrimaryButton(
-                      child: Text("Date Range Picker"),
-                      onPressed: () => DialogCommand(
-                            hint: "Select a date range",
-                            leading: Icon(Icons.airplane),
-                            trailing: Icon(Icons.airplane),
-                            onConfirm: (range) {
-                              print(range);
-                            },
-                          ).open(context))),
-            )),
+        home: ExampleNavigationScreen(),
         theme: ArcaneTheme(
             scheme: ContrastedColorScheme.fromScheme(ColorSchemes.zinc)),
       );
