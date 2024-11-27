81274c3d2f810c923f86d967d56cac95d9563d78
cyberpwn <danielmillst@gmail.com>
2024-11-27 12:37:54 -0500

    Fab group tests

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:arcane/arcane.dart';
 
 bool v = false;
@@ -23,6 +21,84 @@ class ExampleNavigationScreen extends StatelessWidget {
   const ExampleNavigationScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => VFSView(
-      vfs: IOVFS(Directory("${Directory.current.absolute.path}/build")));
+  Widget build(BuildContext context) => FillScreen(
+        child: SizedBox.shrink(),
+        fab: FabGroup(
+          child: Icon(Icons.menu_ionic),
+          children: [
+            FabGroup(
+              horizontal: true,
+              child: Icon(Icons.menu_ionic),
+              children: [
+                FabGroup(
+                  child: Icon(Icons.menu_ionic),
+                  children: [
+                    FabGroup(
+                      horizontal: true,
+                      child: Icon(Icons.menu_ionic),
+                      children: [
+                        Fab(
+                            child: Icon(Icons.airplane),
+                            onPressed: () {
+                              Arcane.closeDrawer(context);
+                            }),
+                        Fab(child: Icon(Icons.activity), onPressed: () {}),
+                        Fab(child: Icon(Icons.alarm), onPressed: () {})
+                      ],
+                    ),
+                    Fab(child: Icon(Icons.activity), onPressed: () {}),
+                    Fab(child: Icon(Icons.alarm), onPressed: () {})
+                  ],
+                ),
+                Fab(child: Icon(Icons.activity), onPressed: () {}),
+                Fab(child: Icon(Icons.alarm), onPressed: () {})
+              ],
+            ),
+            Fab(child: Icon(Icons.activity), onPressed: () {}),
+            Fab(child: Icon(Icons.alarm), onPressed: () {})
+          ],
+        ),
+      );
+}
+
+class FabGroup extends StatelessWidget {
+  final Widget child;
+  final Widget? leading;
+  final List<Widget> children;
+  final bool horizontal;
+
+  const FabGroup(
+      {super.key,
+      required this.child,
+      this.leading,
+      this.children = const [],
+      this.horizontal = false})
+      : assert(children.length > 0, "FabGroup must have at least one child");
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> c = children.toList();
+
+    return Fab(
+      leading: leading,
+      onPressed: () {
+        showPopover(
+            consumeOutsideTaps: true,
+            context: context,
+            offset: horizontal ? const Offset(0, 70) : const Offset(70, 0),
+            barrierDismissable: true,
+            builder: (context) => horizontal
+                ? Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: c,
+                  )
+                : Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: c,
+                  ),
+            alignment: Alignment.bottomRight);
+      },
+      child: child,
+    );
+  }
 }
