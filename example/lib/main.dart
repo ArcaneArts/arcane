5f87dec0803c36d20ee0cfc03c31b6b861977bc3
cyberpwn <danielmillst@gmail.com>
2024-11-27 13:09:50 -0500

    Fab View

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,112 +25,18 @@ class ExampleNavigationScreen extends StatelessWidget {
         child: SizedBox.shrink(),
         fab: FabGroup(
           child: Icon(Icons.menu_ionic),
-          children: [
-            FabGroup(
-              horizontal: true,
-              child: Icon(Icons.menu_ionic),
-              children: [
-                FabGroup(
-                  child: Icon(Icons.menu_ionic),
-                  children: [
-                    FabGroup(
-                      horizontal: true,
-                      child: Icon(Icons.menu_ionic),
-                      children: [
-                        Fab(
-                            child: Icon(Icons.airplane),
-                            onPressed: () {
-                              context.dismissOverlay();
-                            }),
-                        Fab(
-                            child: Icon(Icons.activity),
-                            onPressed: () {
-                              context.dismissOverlay();
-                            }),
-                        Fab(
-                            child: Icon(Icons.alarm),
-                            onPressed: () {
-                              context.dismissOverlay();
-                            })
-                      ],
-                    ),
-                    Fab(
-                        child: Icon(Icons.activity),
-                        onPressed: () {
-                          context.dismissOverlay();
-                        }),
-                    Fab(
-                        child: Icon(Icons.alarm),
-                        onPressed: () {
-                          context.dismissOverlay();
-                        })
-                  ],
-                ),
-                Fab(
-                    child: Icon(Icons.activity),
-                    onPressed: () {
-                      context.dismissOverlay();
-                    }),
-                Fab(
-                    child: Icon(Icons.alarm),
-                    onPressed: () {
-                      context.dismissOverlay();
-                    })
-              ],
-            ),
+          children: (context) => [
             Fab(
                 child: Icon(Icons.activity),
                 onPressed: () {
-                  context.dismissOverlay();
+                  context.dismissFabGroup();
                 }),
             Fab(
                 child: Icon(Icons.alarm),
                 onPressed: () {
-                  context.dismissOverlay();
+                  context.dismissFabGroup();
                 })
           ],
         ),
       );
-}
-
-class FabGroup extends StatelessWidget {
-  final Widget child;
-  final Widget? leading;
-  final List<Widget> children;
-  final bool horizontal;
-
-  const FabGroup(
-      {super.key,
-      required this.child,
-      this.leading,
-      this.children = const [],
-      this.horizontal = false})
-      : assert(children.length > 0, "FabGroup must have at least one child");
-
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> c = children.toList();
-
-    return Fab(
-      leading: leading,
-      onPressed: () {
-        showPopover(
-            consumeOutsideTaps: true,
-            context: context,
-            offset: horizontal ? const Offset(0, 70) : const Offset(70, 0),
-            barrierDismissable: true,
-            builder: (context) => horizontal
-                ? Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: c,
-                  )
-                : Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: c,
-                  ),
-            alignment: Alignment.bottomRight);
-      },
-      child: child,
-    );
-  }
 }
--- lib/component/fab.dart
+++ lib/component/fab.dart
@@ -59,3 +59,66 @@ class Fab extends StatelessWidget {
         ),
       )).blurIn;
 }
+
+class FabGroup extends StatelessWidget {
+  final Widget child;
+  final Widget? leading;
+  final List<Widget> Function(BuildContext) children;
+  final bool horizontal;
+
+  const FabGroup(
+      {super.key,
+      required this.child,
+      this.leading,
+      required this.children,
+      this.horizontal = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Fab(
+      leading: leading,
+      onPressed: () {
+        _PopRef ref = _PopRef();
+        OverlayCompleter o = showPopover(
+            consumeOutsideTaps: true,
+            context: context,
+            offset: horizontal ? const Offset(0, 70) : const Offset(70, 0),
+            barrierDismissable: true,
+            builder: (context) => Pylon<_PopRef>(
+                  value: ref,
+                  builder: (context) => horizontal
+                      ? Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: children(context),
+                        )
+                      : Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: children(context),
+                        ),
+                ),
+            alignment: Alignment.bottomRight);
+        ref.completer = o;
+        o.future.then((value) {
+          context.dismissFabGroup();
+        });
+      },
+      child: child,
+    );
+  }
+}
+
+extension XPrefFabGroupContext on BuildContext {
+  void dismissFabGroup() {
+    Pylon.visiblePylons(this).whereType<Pylon<_PopRef>>().forEach((element) {
+      element.value?.completer?.remove();
+    });
+
+    pylonOr<_PopRef>()?.completer?.remove();
+  }
+}
+
+class _PopRef {
+  OverlayCompleter? completer;
+
+  _PopRef();
+}
