09d8b552cb8b03b70a005fd266495c5d4935937a
cyberpwn <dan@arcane.art>
2024-12-10 05:42:53 -0500

    Flutterjunk

--- arcane_docs/macos/Flutter/GeneratedPluginRegistrant.swift
+++ arcane_docs/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import audioplayers_darwin
 import desktop_drop
 import path_provider_foundation
+import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
 
@@ -15,6 +16,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
