63fa4263574f624d1d3860346323e8f4ff8a0dc3
cyberpwn <dan@arcane.art>
2024-12-11 15:14:07 -0500

    Further reduce dependencies

--- arcane_docs/macos/Flutter/GeneratedPluginRegistrant.swift
+++ arcane_docs/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayers_darwin
 import desktop_drop
 import path_provider_foundation
 import shared_preferences_foundation
@@ -13,7 +12,6 @@ import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
