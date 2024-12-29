e756b3fa34367ac64ec2bebb52cc634b2e48c92b
cyberpwn <dan@arcane.art>
2024-12-28 21:46:35 -0500

    Setup

--- script_sync_docs_live.sh
+++ script_sync_docs_live.sh
@@ -3,3 +3,5 @@ cp arcane_docs/lib/custom.dart docs/lib/custom.dart
 cp arcane_docs/lib/installation_page.dart docs/lib/pages/docs/installation_page.dart
 cp arcane_docs/lib/introduction_page.dart docs/lib/pages/docs/introduction_page.dart
 cp arcane_docs/lib/theme_page.dart docs/lib/pages/docs/theme_page.dart
+
+sed -i '' -e '/^flutter:$/r arcane_docs/shaderinject.txt' -e '/^flutter:$/d' docs/pubspec.yaml
